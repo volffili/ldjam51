d7ed3adcaff58b06781ab82857454a3d38e765f2
Matej <matej.hl@gmail.com>
2022-10-02 14:06:22 +0200

    fix

--- upgrades/black_hole.gd
+++ upgrades/black_hole.gd
@@ -2,11 +2,10 @@ extends "res://upgrades/upgrade.gd"
 
 var black_hole_scene = preload("res://upgrades/black_hole_effect.tscn")
 
-var impact = false
-
 func _on_shoot(shot):
 	var b = black_hole_scene.instantiate()
 	shot.add_child(b)
+	var impact = false
 	shot.connect("impact", func(shot, enemy): 
 		if impact:
 			return
