3e377a724518e1b19c98e296740279517d3e1d06
Matej <matej.hl@gmail.com>
2022-10-02 17:45:12 +0200

    charge up

---
+++ upgrades/charge_up.gd
@@ -0,0 +1,15 @@
+extends "res://upgrades/upgrade.gd"
+
+var effect_scene = preload("res://upgrades/charge_up_effect.tscn")
+var effect
+
+func _on_picked():
+	player.charge_up = true
+	effect = effect_scene.instantiate()
+	player.add_child(effect)
+	
+func _process(delta):
+	if effect == null:
+		return
+	effect.get_node("Particles").emitting = Input.is_action_pressed("shoot")
+		
